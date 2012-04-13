dc90dcc72f776cbf04536930828ca15227b1c620
hellspawninf <hellspawninf@8bf067b4-e61f-4fb1-997d-9c56b3efd43b>
2012-04-13 03:51:38 +0000

    Recycler fixes (Super-Man is a noob) Chat fixes. DirectoryServer updates (loads from zonelist.xml for each request now) Removed some silly debug code.. Added optional updates. Account server updates (little more stable now)

--- branches/MiniAccountServer/MiniAccountServer/Listener.cs
+++ branches/MiniAccountServer/MiniAccountServer/Listener.cs
@@ -23,159 +23,180 @@ namespace MiniAccountServer
             client = new DatabaseClient();
             listener = new HttpListener();
 
-            foreach(string prefix in prefixes)
-            {
-                listener.Prefixes.Add(prefix);
-            }
+            listener.Prefixes.Add("http://*:1010/");
         }
 
         public void Start()
         {
-            listener.Start();
-            while (true)
+            if (!HttpListener.IsSupported)
             {
-                HttpListenerContext context = listener.GetContext();
-                HandleRequest(context);
+                Console.WriteLine("Not supported");
             }
+            else
+            {
 
-            listener.Close();
+                try
+                {
+                    listener.Start();
+                    while (true)
+                    {
+                        HttpListenerContext context = listener.GetContext();
+                        HandleRequest(context);
+                    }
+                }
+                catch (Exception e)
+                {
+                    listener.Close();
+                }
+            }
         }
 
         private void HandleRequest(HttpListenerContext context)
         {
             HttpListenerRequest request = context.Request;
             HttpListenerResponse response = context.Response;
-            
 
-            // Lets figure out the request...
-            switch(request.HttpMethod)
+            Console.WriteLine("Request {0}/{1} from {2}", request.RawUrl.ToString(), request.HttpMethod, request.RemoteEndPoint);
+
+            try
             {
-                // Sanity check request
-                case "GET":
-                    byte[] responseString = Encoding.UTF8.GetBytes("Works!");
-                    response.ContentLength64 = responseString.Length;
-                    response.OutputStream.Write(responseString, 0, responseString.Length);
-                    response.OutputStream.Close();
-                    break;
 
-                // Account registration request
-                case "PUT":
+                // Lets figure out the request...
+                switch (request.HttpMethod)
+                {
+                    // Sanity check request
+                    case "GET":
+                        byte[] responseString = Encoding.UTF8.GetBytes("Works!");
+                        response.ContentLength64 = responseString.Length;
+                        response.OutputStream.Write(responseString, 0, responseString.Length);
+                        response.OutputStream.Close();
+                        break;
 
-                    // 1. Is the request good?
-                    if(!request.HasEntityBody)
-                    {
-                        response.StatusCode = 400;
+                    // Account registration request
+                    case "PUT":
+
+                        // 1. Is the request good?
+                        if (!request.HasEntityBody)
+                        {
+                            response.StatusCode = 400;
+                            response.OutputStream.Close();
+
+                            break;
+                        }
+
+                        string registrationData = new StreamReader(request.InputStream).ReadToEnd();
+                        var regModel = JsonConvert.DeserializeObject<Account.RegistrationRequestModel>(registrationData);
+
+                        if (!regModel.IsRequestValid())
+                        {
+                            response.StatusCode = 400;
+                            response.OutputStream.Close();
+
+                            break;
+                        }
+
+
+                        // 2. Is the username available?
+                        if (client.UsernameExists(regModel.Username))
+                        {
+                            response.StatusCode = 403;
+                            response.OutputStream.Close();
+
+                            break;
+                        }
+
+
+                        // 3. Are the credentials good?
+                        if (!Account.IsValidUsername(regModel.Username) || !Account.IsValidEmail(regModel.Email))
+                        {
+                            response.StatusCode = 406;
+                            response.OutputStream.Close();
+
+                            break;
+                        }
+
+
+                        // 4. Add it to the database, and we're good to go!
+                        var account = client.AccountCreate(regModel.Username, regModel.PasswordHash,
+                                                           Guid.NewGuid().ToString(),
+                                                           DateTime.Now, DateTime.Now, 0, regModel.Email);
+
+
+                        // 5. Oh uh? Some error happened!
+                        if (account == null)
+                        {
+                            response.StatusCode = 500;
+                            response.OutputStream.Close();
+
+                            break;
+                        }
+
+
+                        // Done!
+                        response.StatusCode = 201;
+                        response.OutputStream.Close();
+                        break;
+
+                    // Account login request
+                    case "POST":
+                        // 1. Is the request good?
+                        if (!request.HasEntityBody)
+                        {
+                            response.StatusCode = 400;
+                            response.OutputStream.Close();
+
+                            break;
+                        }
+
+                        string loginData = new StreamReader(request.InputStream).ReadToEnd();
+                        var loginModel = JsonConvert.DeserializeObject<Account.LoginRequestModel>(loginData);
+
+                        if (!loginModel.IsRequestValid())
+                        {
+                            response.StatusCode = 400;
+                            response.OutputStream.Close();
+
+                            break;
+                        }
+
+
+                        // 2. Are the credentials good?
+                        if (!client.IsAccountValid(loginModel.Username, loginModel.PasswordHash))
+                        {
+                            response.StatusCode = 404;
+                            response.OutputStream.Close();
+
+                            break;
+                        }
+
+
+                        // 3. Good to go!
+                        Account a = client.AccountLogin(loginModel.Username, loginModel.PasswordHash, request.RemoteEndPoint.Address.ToString());
+
+                        var loginResponseModel = new Account.LoginResponseModel();
+                        loginResponseModel.Username = a.Username;
+                        loginResponseModel.Email = a.Email;
+                        loginResponseModel.TicketId = a.SessionId;
+                        loginResponseModel.DateCreated = a.DateCreated;
+                        loginResponseModel.LastAccessed = a.LastAccessed;
+                        loginResponseModel.Permission = a.Permission;
+
+                        var loginResponseString = JsonConvert.SerializeObject(loginResponseModel);
+                        byte[] loginResponseData = Encoding.UTF8.GetBytes(loginResponseString);
+                        response.StatusCode = 200;
+                        response.ContentLength64 = loginResponseData.Length;
+                        response.OutputStream.Write(loginResponseData, 0, loginResponseData.Length);
                         response.OutputStream.Close();
 
                         break;
-                    }
-
-                    string registrationData = new StreamReader(request.InputStream).ReadToEnd();
-                    var regModel = JsonConvert.DeserializeObject<Account.RegistrationRequestModel>(registrationData);
-
-                    if (!regModel.IsRequestValid())
-                    {
-                        response.StatusCode = 400;
-                        response.OutputStream.Close();
-
-                        break;
-                    }
-
-                    
-                    // 2. Is the username available?
-                    if (client.UsernameExists(regModel.Username))
-                    {
-                        response.StatusCode = 403;
-                        response.OutputStream.Close();
-
-                        break;
-                    }
-
-
-                    // 3. Are the credentials good?
-                    if (!Account.IsValidUsername(regModel.Username) || !Account.IsValidEmail(regModel.Email))
-                    {
-                        response.StatusCode = 406;
-                        response.OutputStream.Close();
-
-                        break;
-                    }
-
-
-                    // 4. Add it to the database, and we're good to go!
-                    var account = client.AccountCreate(regModel.Username, regModel.PasswordHash,
-                                                       Guid.NewGuid().ToString(),
-                                                       DateTime.Now, DateTime.Now, 0, regModel.Email);
-
-
-                    // 5. Oh uh? Some error happened!
-                    if (account == null)
-                    {
-                        response.StatusCode = 500;
-                        response.OutputStream.Close();
-
-                        break;
-                    }
-
-
-                    // Done!
-                    response.StatusCode = 201;
-                    response.OutputStream.Close();
-                    break;
-
-                // Account login request
-                case "POST":
-
-                    // 1. Is the request good?
-                    if (!request.HasEntityBody)
-                    {
-                        response.StatusCode = 400;
-                        response.OutputStream.Close();
-
-                        break;
-                    }
-
-                    string loginData = new StreamReader(request.InputStream).ReadToEnd();
-                    var loginModel = JsonConvert.DeserializeObject<Account.LoginRequestModel>(loginData);
-
-                    if (!loginModel.IsRequestValid())
-                    {
-                        response.StatusCode = 400;
-                        response.OutputStream.Close();
-
-                        break;
-                    }
-
-
-                    // 2. Are the credentials good?
-                    if (!client.IsAccountValid(loginModel.Username, loginModel.PasswordHash))
-                    {
-                        response.StatusCode = 404;
-                        response.OutputStream.Close();
-
-                        break;
-                    }
-
-
-                    // 3. Good to go!
-                    Account a = client.AccountLogin(loginModel.Username, loginModel.PasswordHash);
-
-                    var loginResponseModel = new Account.LoginResponseModel();
-                    loginResponseModel.Username = a.Username;
-                    loginResponseModel.Email = a.Email;
-                    loginResponseModel.TicketId = a.SessionId;
-                    loginResponseModel.DateCreated = a.DateCreated;
-                    loginResponseModel.LastAccessed = a.LastAccessed;
-                    loginResponseModel.Permission = a.Permission;
-
-                    var loginResponseString = JsonConvert.SerializeObject(loginResponseModel);
-                    byte[] loginResponseData = Encoding.UTF8.GetBytes(loginResponseString);
-                    response.StatusCode = 200;
-                    response.ContentLength64 = loginResponseData.Length;
-                    response.OutputStream.Write(loginResponseData, 0, loginResponseData.Length);
-                    response.OutputStream.Close();
-
-                    break;
+                }
+            }
+            catch (Exception e)
+            {
+                if (e.Message.Contains("SQL"))
+                {
+                Console.WriteLine("Unhandled SQL exception");
+                }
             }
         }
     }
