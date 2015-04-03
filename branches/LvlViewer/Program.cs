aa58fb5452f557d43e9ffad50478910b88b95f3e
mizzouse <mizzouse@8bf067b4-e61f-4fb1-997d-9c56b3efd43b>
2015-04-03 01:48:23 +0000

    Updated the level viewer to output to a text file. Changed energy and repair rates for computers, now repairs if active Added ricks physic color finder into the project, updated all asset physic tile locators Added the *find command to show who's online possibly under a different alias

--- branches/LvlViewer/Program.cs
+++ branches/LvlViewer/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using Assets;
 
 namespace LvlViewer
@@ -9,6 +10,7 @@ namespace LvlViewer
     /// </summary>
     class Program
     {
+        private static StreamWriter fileOut;
         /// <summary>
         /// 0-31 english-friendly values for each type of physics overlay.
         /// </summary>
@@ -16,21 +18,13 @@ namespace LvlViewer
             new List<string>
             {
                 "Clear",
-
                 "Red Solid","Red Upper Left","Red Upper Right","Red Lower Left","Red Lower Right",
-
                 "Green Solid","Green Upper Left","Green Upper Right","Green Lower Left","Green Lower Right",
-
                 "Yellow Solid","Yellow Upper Left","Yellow Upper Right","Yellow Lower Left","Yellow Lower Right",
-
                 "Orange Solid","Orange Upper Left","Orange Upper Right","Orange Lower Left","Orange Lower Right",
-
                 "Purple Solid","Purple Upper Left","Purple Upper Right","Purple Lower Left","Purple Lower Right",
-
                 "Red Move Right", "Red Move Left", "Red Move Down", "Red Move Up",
-
                 "Teal Solid",
-
                 "Blue Solid"
             };
 
@@ -40,37 +34,45 @@ namespace LvlViewer
         /// <param name="args">args[0] must be the level file.</param>
         static void Main(string[] args)
         {
-            if(args.Length == 0)
+            if (args.Length == 0)
             {
-                throw new ArgumentException("No .lvl file given");
+                Console.WriteLine("No .lvl file given.");
+                System.Threading.Thread.Sleep(5000);
             }
+            else
+            {
+                LvlInfo level = LvlInfo.Load(args[0]);
+                fileOut = new StreamWriter(new FileStream("Results.txt", FileMode.Create));
 
-            LvlInfo level = LvlInfo.Load(args[0]);
+                Print("File Opened", delegate { PrintValue("Name", args[0]); });
 
-            Print("File Opened", delegate { PrintValue("Name", args[0]); } );
+                Print("General",
+                    delegate
+                    {
+                        PrintValue("Width", level.Width);
+                        PrintValue("Height", level.Height);
+                    });
 
-            Print("General", 
-                delegate
-                {
-                    PrintValue("Width", level.Width);
-                    PrintValue("Height", level.Height);
-                });
+                Print("Physics",
+                    delegate
+                    {
+                        PrintValue("Low", level.PhysicsLow);
+                        PrintValue("High", level.PhysicsHigh);
+                    });
 
-            Print("Physics", 
-                delegate
-                {
-                    PrintValue("Low", level.PhysicsLow);
-                    PrintValue("High", level.PhysicsHigh);
-                });
-
-            Print("Blob References (Filename, Id)", 
-                delegate
+                Print("Blob References (Filename, Id)",
+                    delegate
                     {
                         PrintValue("Object Blobs", level.ObjectBlobs);
                         PrintValue("Floor Blobs", level.FloorBlobs);
                     });
 
-            Print("Tile References (Terrain#, UNKNOWN, Physics/Vision)",delegate{ PrintValue("Tiles", level.Tiles); });
+                Print("Tile References (Terrain#, UNKNOWN, Physics/Vision)", 
+                    delegate 
+                    { PrintValue("Tiles", level.Tiles); });
+                fileOut.Close();
+                fileOut.Dispose();
+            }
         }
 
         static void Print(string title, Action action)
@@ -78,11 +80,15 @@ namespace LvlViewer
             Console.WriteLine("===== " + title + " =====");
             action();
             Console.WriteLine();
+
+            fileOut.WriteLine("===== " + title + " =====");
+            fileOut.WriteLine();
         }
 
         static void PrintValue<T>(string name, T value)
         {
             Console.WriteLine("[" + name + "]\t" + value);
+            fileOut.WriteLine("[" + name + "]\t" + value);
         }
 
         static void PrintValue<T>(string name, T[] values)
@@ -92,6 +98,7 @@ namespace LvlViewer
             for(int idx = 0; idx < values.Length; idx++)
             {
                 Console.Write("\t");
+                fileOut.Write("\t");
                 PrintValue(idx.ToString(), values[idx]);
             }
         }
@@ -102,6 +109,7 @@ namespace LvlViewer
             for(int idx = 0; idx < values.Length; idx++)
             {
                 Console.Write("\t");
+                fileOut.Write("\t");
                 PrintValue(idx.ToString(), values[idx].FileName + "\t\t" + values[idx].Id);
             }
         }
@@ -113,8 +121,9 @@ namespace LvlViewer
             for(int idx = 0; idx < values.Length; idx++)
             {
                 String s = String.Format("{0,-12}{1,-10}{2}", "Terrain" + values[idx].Unknown0, values[idx].Unknown1,
-                                         physValues[values[idx].Unknown2 & 0x1F] + "/" + (values[idx].Unknown2 >> 5));
+                                         physValues[values[idx].PhysicsVision & 0x1F] + "/" + (values[idx].PhysicsVision >> 5));
                 Console.Write("\t");
+                fileOut.Write("\t");
                 PrintValue(idx.ToString(), s);
             }
         }
